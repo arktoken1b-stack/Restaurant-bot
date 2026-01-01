2ed6b73b00d8f3308cd0ff64ecb9d7fe7e7b0ffd
arktoken1b-stack <arktoken1b@gmail.com>
2026-01-01 16:35:54 -0500

    Update pedido_model.py

--- models/pedido_model.py
+++ models/pedido_model.py
@@ -1,1 +1,18 @@
 
+from database.db import get_connection
+
+def crear_pedido(chat_id):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        "INSERT INTO pedidos (chat_id, estado) VALUES (%s, %s) RETURNING id",
+        (chat_id, "creando")
+    )
+    pedido_id = cur.fetchone()[0]
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return pedido_id
