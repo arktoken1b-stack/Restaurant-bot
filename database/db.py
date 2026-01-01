7e3d8455f26f8d595eee66a10a95b76b11940315
arktoken1b-stack <arktoken1b@gmail.com>
2026-01-01 12:51:53 -0500

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,7 +1,8 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.getenv("postgresql://restaurant_db_w2yj_user:r3PK7YSftqvfmCNULcZ6Eku5oEavhBuc@dpg-d5baukuuk2gs73f64tog-a.virginia-postgres.render.com/restaurant_db_w2yj")
+# Leer la URL desde la variable de entorno que configuraste en Render
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_connection():
     return psycopg2.connect(DATABASE_URL)
