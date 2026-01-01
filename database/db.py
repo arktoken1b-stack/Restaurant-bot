7d5b5046bffa142251178b944261616d785c2e68
arktoken1b-stack <arktoken1b@gmail.com>
2026-01-01 12:49:50 -0500

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("postgresql://restaurant_db_w2yj_user:r3PK7YSftqvfmCNULcZ6Eku5oEavhBuc@dpg-d5baukuuk2gs73f64tog-a.virginia-postgres.render.com/restaurant_db_w2yj")
 
 def get_connection():
     return psycopg2.connect(DATABASE_URL)
