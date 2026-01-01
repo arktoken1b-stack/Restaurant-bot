e1bac046c95cae4bd1137a72ef545054e53a1c6b
arktoken1b-stack <arktoken1b@gmail.com>
2026-01-01 12:43:21 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
+import os
+import discord
+from discord.ext import commands
+from database.init_db import init_db
 
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    init_db()
+    print(f"Bot conectado como {bot.user}")
+
+# Cargamos comandos
+bot.load_extension("commands.pedido")
+
+bot.run(os.getenv("DISCORD_TOKEN"))
