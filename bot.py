58d46e2bf4eb23e8cdbad06eeb3a453fb8611a91
arktoken1b-stack <arktoken1b@gmail.com>
2026-01-01 12:45:53 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,20 @@
 import os
 import discord
 from discord.ext import commands
+from discord import app_commands
 from database.init_db import init_db
 
 intents = discord.Intents.default()
-intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     init_db()
+    # Sincronizar comandos de barra
+    await bot.tree.sync()
     print(f"Bot conectado como {bot.user}")
 
-# Cargamos comandos
+# Cargar cog de pedidos
 bot.load_extension("commands.pedido")
 
 bot.run(os.getenv("DISCORD_TOKEN"))
