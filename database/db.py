6063aabbdb4389f246fa5241d3d6a1ce3ec3d95c
arktoken1b-stack <arktoken1b@gmail.com>
2026-01-01 16:31:17 -0500

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,8 +1,5 @@
-import os
 import psycopg2
-
-# Leer la URL desde la variable de entorno que configuraste en Render
-DATABASE_URL = os.getenv("DATABASE_URL")
+from config import DATABASE_URL
 
 def get_connection():
     return psycopg2.connect(DATABASE_URL)
