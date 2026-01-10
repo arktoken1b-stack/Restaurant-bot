23105ef7d07dfe9629276b0a573f761798df15bd
arktoken1b-stack <arktoken1b@gmail.com>
2026-01-10 14:49:46 -0500

    Delete handlers directory

--- handlers/mensajes.py
+++
@@ -1,9 +0,0 @@
-MENSAJE_INICIO_PEDIDO = (
-    "🧾 *Nuevo pedido*\n"
-    "Selecciona los platos que deseas:"
-)
-
-MENSAJE_PLATO_AGREGADO = "🍽 Plato agregado al pedido"
-MENSAJE_SIGUIENTE = "➡️ Continuando con el pedido..."
-
-MENSAJE_ERROR = "❌ Ocurrió un error, intenta nuevamente."
